4ae134ef6d444285cc05532bbd4b26437d8e95b4
Unknown <lee.david.cs@gmail.com>
2019-04-08 22:24:12 -0700

    Switch test framework from mocha to jest

--- tests/server.test.ts
+++ tests/server.test.ts
@@ -1,28 +1,33 @@
-import { expect } from "chai";
 import config from "config";
 import Http from "http";
 import { Server } from "../src/server";
 
 const port: number = config.get<number>("port");
 
+const INIT_SERVER_SPIN_TIMEOUT: number = 120000;
+
 describe("Server", () => {
 	const server: Server = new Server(port);
 
-	afterEach(() => {
-		server.stop();
+	afterEach(async () => {
+		await server.stop();
 	});
 
 	it("Should be a constructor", () => {
-		expect(typeof Server).to.equal("function");
+		expect(typeof Server).toBe("function");
 	});
 
-	it("Should run the server", async () => {
-		await server.run();
+	it(
+		"Should run the server",
+		async () => {
+			await server.run();
 
-		expect(server.isRunning).to.equal(true);
-	});
+			expect(server.isRunning).toBe(true);
+		},
+		INIT_SERVER_SPIN_TIMEOUT
+	);
 
 	it("Should return an instance of an http server", () => {
-		expect(server.instance instanceof Http.Server).to.equal(true);
+		expect(server.instance instanceof Http.Server).toBe(true);
 	});
 });
